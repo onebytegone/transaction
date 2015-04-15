1d800a6b4172bce75872da8c3e573cf24a7fc256
Ethan Smith <ethan@onebytegone.com>
2015-04-15 16:24:53 -0400

    Refactored OutputGenerator

--- src/OutputGenerator.php
+++ src/OutputGenerator.php
@@ -10,29 +10,41 @@
 
 class OutputGenerator {
    public static function printOutput($module, $package) {
-      echo '<div class="transaction-step">';
-
-      echo "<h1>".get_class($module)."</h1>";
-
-      self::printModule($module);
-      self::printPackage($package);
-
-      echo '<div class="clear"></div>';
-      echo '</div>';
+      echo self::summarize($module, $package);
    }
 
    public static function printModule($module) {
-      echo '<div class="module">';
-      echo '<h3>Config:</h3>';
-      echo self::generateTableFromArray(get_object_vars($module));
-      echo '</div>';
+      echo self::summarizeModule($module);
    }
 
    public static function printPackage($package) {
-      echo '<div class="package">';
-      echo '<h3>Package:</h3>';
-      echo self::generateTableFromArray($package);
-      echo '</div>';
+      echo self::summarizePackage($package);
+   }
+
+   public static function summarize($module, $package) {
+      $output = '<div class="transaction-step">';
+      $output .= "<h1>".get_class($module)."</h1>";
+      $output .= self::summarizeModule($module);
+      $output .= self::summarizePackage($package);
+      $output .= '<div class="clear"></div>';
+      $output .= '</div>';
+      return $output;
+   }
+
+   public static function summarizeModule($module) {
+      $output = '<div class="module">';
+      $output .= '<h3>Config:</h3>';
+      $output .= self::generateTableFromArray(get_object_vars($module));
+      $output .= '</div>';
+      return $output;
+   }
+
+   public static function summarizePackage($package) {
+      $output = '<div class="package">';
+      $output .= '<h3>Package:</h3>';
+      $output .= self::generateTableFromArray($package);
+      $output .= '</div>';
+      return $output;
    }
 
    private static function generateTableFromArray($values) {

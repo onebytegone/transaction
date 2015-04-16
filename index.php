c0f3abce65ca80b75b409965ec4fab549ab627e9
Ethan Smith <ethan@onebytegone.com>
2015-04-16 10:45:05 -0400

    Updated try form

--- index.php
+++ index.php
@@ -7,6 +7,16 @@
 <body>
    <h1>Object Transactions</h1>
    <p></p>
+   <div>
+      <h2>Try</h2>
+      <form action="#">
+         <div><span>Principal:</span><input type="text" value="<?php echo $_GET['principal'] ?>" name="principal"></div>
+         <div><span>Interest Rate:</span><input type="text" value="<?php echo $_GET['rate'] ?>" name="rate"></div>
+         <div><span>Times compounded:</span><input type="text" value="<?php echo $_GET['compound'] ?>" name="compound"></div>
+         <div><span>Years:</span><input type="text" value="<?php echo $_GET['years'] ?>" name="years"></div>
+         <div><input type="submit" value="Calculate"></div>
+      </form>
+   </div>
    <?php require 'src/demo.php'; ?>
 </body>
 </html>
